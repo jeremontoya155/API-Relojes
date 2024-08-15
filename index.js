b1ace57f7db82d9aa5a7768a89080313367d7f51
Jeremias <jeremontoya155@gmail.com>
2024-08-15 08:07:06 -0300

    Subida Español con datos nuevos a webs

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const cors = require('cors');
 const ZKLib = require('node-zklib');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Utiliza el puerto configurado en las variables de entorno o el puerto 3000 por defecto
 const securityKey = process.env.SECURITY_KEY;
 
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(cors()); // Permitir solicitudes desde cualquier origen
 
+// Middleware para verificar la clave de seguridad en las rutas protegidas
 const verifyApiKey = (req, res, next) => {
     const apiKey = req.query.api_key;
     if (apiKey && apiKey === securityKey) {
@@ -20,6 +21,7 @@ const verifyApiKey = (req, res, next) => {
     }
 };
 
+// Cargar las sucursales desde las variables de entorno
 const branches = [
     {
         ip: process.env.BRANCH_0_IP,
@@ -31,6 +33,7 @@ const branches = [
     }
 ];
 
+// Ruta para obtener datos de asistencia (protegida con clave de seguridad)
 app.get('/fetch-attendance', verifyApiKey, async (req, res) => {
     const branchQuery = req.query.branch;
     let selectedBranches = branches;
@@ -87,13 +90,15 @@ app.get('/fetch-attendance', verifyApiKey, async (req, res) => {
     }
 });
 
+// Ruta para la documentación (sin requerir clave de seguridad)
 app.get('/documentation', (req, res) => {
     res.render('documentation', {
-        apiUrl: `http://localhost:${port}/fetch-attendance`,
+        apiUrl: `https://api-relojes-production.up.railway.app/fetch-attendance`,
         apiKey: securityKey
     });
 });
 
+// Iniciar el servidor
 app.listen(port, () => {
     console.log(`Attendance API running at http://localhost:${port}`);
 });
